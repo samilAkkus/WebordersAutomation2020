c63a254eea076568cae2a94edeeda73d66b9ffe4
samilAkkus <samilakgus15@email.com>
2020-04-08 02:35:32 -0700

    new update

--- src/test/java/com/weborders/pages/AbstractBasePage.java
+++ src/test/java/com/weborders/pages/AbstractBasePage.java
@@ -1,8 +1,12 @@
 package com.weborders.pages;
 
 import com.weborders.utilities.Driver;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 /**
@@ -16,10 +20,31 @@ public abstract class AbstractBasePage {
     protected WebDriverWait wait = new WebDriverWait(driver,20);
     //we use protected coz this class meant to be extended and we will use it in sub classes
 
+    @FindBy(tagName = "h1")
+    protected WebElement pageLogo;
+
+    public String getPageLogoText(){
+
+        return pageLogo.getText();
+
+    }
+
     public AbstractBasePage(){
 
         PageFactory.initElements(driver,this);
         //to locate annotations
+
+    }
+
+    /**
+     * Specify component name as a parameter, like --> View all products or Order
+     * @param component
+     */
+    public void navigateTo(String component){
+
+        String locator = "//a[text()='"+component+"']";
+
+        wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath(locator))).click();
 
     }
 }
